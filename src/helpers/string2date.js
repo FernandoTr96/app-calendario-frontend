231f3d2d82afcfc92683d5a52987eb779deac83b
FernandoTr96 <fernando.tr96@outlook.es>
2021-11-18 14:13:09 -0600

    string2date fixed

--- src/helpers/string2date.js
+++ src/helpers/string2date.js
@@ -5,7 +5,7 @@ export const string2date = (events = [])=>{
     return events.map((event)=>({
         ...event,
         start: moment(event.start).toDate(),
-        end: moment(event.start).toDate()
+        end: moment(event.end).toDate()
     }))
 
 }
